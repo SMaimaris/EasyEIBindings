08137970f6203a892f0db4b8a82e62e839d63106
Stylianos Maimaris <s_maimaris@wargaming.net>
2026-01-15 15:48:33 +0200

    Copyright notice

--- Source/EasyEIBindings/Private/EasyEIBindingsDeveloperSettings.cpp
+++ Source/EasyEIBindings/Private/EasyEIBindingsDeveloperSettings.cpp
@@ -1,4 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Copyright Stylianos Maimaris. All Rights Reserved.
 
 
 #include "EasyEIBindingsDeveloperSettings.h"
